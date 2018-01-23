2374957c6d43bfee4242755a55f05de684ca2b10
Andrey Murzich <Andrey_Murzich@epam.com>
2018-01-23 17:03:54 +0300

    DSC 95

--- dsc/iis.ps1
+++ dsc/iis.ps1
@@ -1,13 +1,31 @@
 Configuration InstallIIS
 {
 	param (
-		[string]$machineName
+		[string]$machineName,
+		[string]$bindPort
 	)
-	Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+	
+	Import-DscResource -ModuleName PSDesiredStateConfiguration,xWebAdministration
+	
 	Node $machineName {
 		WindowsFeature IIS {
 			Ensure = "Present"
 			Name = "Web-Server"
+		}
+		WindowsFeature IISConsole {
+			Ensure = "Present"
+			Name = "Web-Mgmt-Console"
+		}
+		xWebsite DefaultSite {
+			Ensure = "Present"
+			Name = "Default Web Site"
+			State = "Started"
+			PhysicalPath = "C:\inetpub\wwwroot"
+			DependsOn = "[WindowsFeature]IIS"
+			BindingInfo = MSFT_xWebBindingInformation {
+				Protocol = "HTTP"
+				Port = $bindPort
+			}
 		}
 	}
 }
