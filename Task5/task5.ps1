327331338a052a4d871a5af19d70cef1817f44fc
Andrey Murzich <Andrey_Murzich@epam.com>
2018-01-26 15:50:15 +0300

    Task 5 1

---
+++ Task5/task5.ps1
@@ -0,0 +1,27 @@
+﻿# Andrey Murzich
+# Task 5
+# Runbook deployment script
+
+# Global configuration
+$envLocation = "West Europe"
+$templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task5/index.json"
+$templateParameterFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task5/parameters.json"
+$envPrefix = "learning4"
+$credTemplateLink = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task5/cred.json"
+$runbookTemplateLink = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task5/runbook.json"
+$jobTemplateLink = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task5/job.json"
+
+# Account Login
+Login-AzureRmAccount
+
+# Resource Group Deployment
+New-AzureRmResourceGroup -Name $envPrefix -Location $envLocation -Force
+New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix `
+                                   -TemplateFile $templateFile `
+                                   -TemplateParameterFile $templateParameterFile `
+                                   -envPrefix $envPrefix `
+                                   -envLocation $envLocation `
+                                   -credentialsTemplateLink $credTemplateLink `
+                                   -runbookTemplateLink $runbookTemplateLink `
+                                   -jobTemplateLink $jobTemplateLink `
+                                   -Force -Verbose
