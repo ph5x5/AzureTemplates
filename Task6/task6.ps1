464734e0705eede3d97c4793df210b88ddfe00d8
Andrey Murzich <Andrey_Murzich@epam.com>
2018-02-05 14:44:59 +0300

    Task6 5

--- Task6/task6.ps1
+++ Task6/task6.ps1
@@ -2,7 +2,7 @@
 # Task 6
 # Azure Web Applications deployment
 
-# Template configuration
+# Web App templates configuration
 $templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/index.json"
 $templateParameterUri1 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/we/parameters.json"
 $templateParameterUri2 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/ne/parameters.json"
@@ -10,6 +10,11 @@ $envPrefix1 = "learning6-we"
 $envPrefix2 = "learning6-ne"
 $envLocation1 = "West Europe"
 $envLocation2 = "North Europe"
+
+# Traffic manager profile configuration
+$tmTemplateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/trafman.json"
+$tmTemplaterParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/tmparameters.json"
+$tmEnvLocation = "global"
 
 # Account Login
 #Login-AzureRmAccount
@@ -26,4 +31,9 @@ New-AzureRmResourceGroup -Name $envPrefix2 -Location $envLocation2 -Force
 New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix2 `
                                    -TemplateFile $templateFile `
                                    -TemplateParameterUri $templateParameterUri2 `
+                                   -Force -Verbose
+# Traffic manager
+New-AzureRmResourceGroupDeployment -ResourceGroupName $tmEnvLocation `
+                                   -TemplateFile $tmTemplateFile `
+                                   -TemplateParameterUri $tmTemplateParameterUri `
                                    -Force -Verbose
