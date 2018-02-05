66b238cb1d79b4915a5c9de1bae6cd4388fb506a
Andrey Murzich <Andrey_Murzich@epam.com>
2018-02-05 18:02:47 +0300

    Task6 14

--- Task6/task6.ps1
+++ Task6/task6.ps1
@@ -2,40 +2,23 @@
 # Task 6
 # Azure Web Applications deployment
 
-# Web App templates configuration
+# Templates configuration
 $templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/index.json"
-$templateParameterUri1 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/we/parameters.json"
-$templateParameterUri2 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/ne/parameters.json"
-$envPrefix1 = "learning6-we"
-$envPrefix2 = "learning6-ne"
-$envLocation1 = "West Europe"
-$envLocation2 = "North Europe"
-
-# Traffic manager profile configuration
-$envPrefix3 = "learning6-gen"
-$tmTemplateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/trafman.json"
-$tmTemplateParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/tmparameters.json"
-$tmEnvLocation = "global"
+$templateParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/parameters.json"
+$envPrefix = "learning6"
+$envLocation = "West Europe"
 
 # Account Login
 #Login-AzureRmAccount
 
 # Resource Group Deployment
-# WebSite1
-New-AzureRmResourceGroup -Name $envPrefix1 -Location $envLocation1 -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix1 `
+New-AzureRmResourceGroup -Name $envPrefix -Location $envLocation -Force
+New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix `
                                    -TemplateFile $templateFile `
-                                   -TemplateParameterUri $templateParameterUri1 `
+                                   -TemplateParameterUri $templateParameterUri `
                                    -Force -Verbose
-# WebSite2
-New-AzureRmResourceGroup -Name $envPrefix2 -Location $envLocation2 -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix2 `
-                                   -TemplateFile $templateFile `
-                                   -TemplateParameterUri $templateParameterUri2 `
-                                   -Force -Verbose
-# Traffic manager
-New-AzureRmResourceGroup -Name $envPrefix3 -Location $envLocation1 -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix3 `
-                                   -TemplateFile $tmTemplateFile `
-                                   -TemplateParameterUri $tmTemplateParameterUri `
-                                   -Force -Verbose
+
+# Info
+$tm = Get-AzureRmTrafficManagerProfile -ResourceGroupName $envPrefix
+$tm
+$tm.Endpoints
