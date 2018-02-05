6c2b6f853fa34bdae41c4d94f0411009c0c5ae87
Andrey Murzich <Andrey_Murzich@epam.com>
2018-02-05 14:19:28 +0300

    Task6 1

--- Task6/task6.ps1
+++ Task6/task6.ps1
@@ -3,20 +3,28 @@
 # Azure Web Applications deployment
 
 # Template configuration
-$templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/index.json"
-$templateParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/parameters.json"
+$templateFile1 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/we/index.json"
+$templateParameterUri1 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/we/parameters.json"
+$templateFile2 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/ne/index.json"
+$templateParameterUri2 = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task6/ne/parameters.json"
 $envPrefix1 = "learning6-we"
 $envPrefix2 = "learning6-ne"
 $envLocation1 = "West Europe"
 $envLocation2 = "North Europe"
 
 # Account Login
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 # Resource Group Deployment
+# WebSite1
 New-AzureRmResourceGroup -Name $envPrefix1 -Location $envLocation1 -Force
+New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix1 `
+                                   -TemplateFile $templateFile1 `
+                                   -TemplateParameterUri $templateParameterUri1 `
+                                   -Force -Verbose
+# WebSite2
 New-AzureRmResourceGroup -Name $envPrefix2 -Location $envLocation2 -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix `
-                                   -TemplateFile $templateFile `
-                                   -TemplateParameterUri $templateParameterUri `
+New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix2 `
+                                   -TemplateFile $templateFile2 `
+                                   -TemplateParameterUri $templateParameterUri2 `
                                    -Force -Verbose
