08910db8871b9400ba6d456327af0a2a71753b23
ihar.kotau <Ihar.Kotau@thomsonreuters.com>
2018-02-09 13:38:34 +0300

    Task7 29

--- Task7/restore.ps1
+++ Task7/restore.ps1
@@ -10,7 +10,10 @@ $endDate = Get-Date
 $resourceGroupStorage = "cloud-shell-storage-westeurope"
 $storageAccountName = "csbf904e53bc8b7x4111x93b"
 
-$container = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM -Status Registered -FriendlyName "$envPrefix-vm"
+# Account Login
+#Login-AzureRmAccount
+
+$container = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM -Status Registered -FriendlyName "AzureBackup_$envPrefix-vm"
 $backupItem = Get-AzureRmRecoveryServicesBackupItem -WorkloadType AzureVM -Container $container
 $RP = Get-AzureRmRecoveryServicesBackupRecoveryPoint -Item $backupItem -StartDate $startDate.ToUniversalTime() -EndDate $endDate.ToUniversalTime()
 $RestoreJob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $RP[0] -StorageAccountName $storageAccountName -StorageAccountResourceGroupName $resourceGroupStorage
--- Task8/task8.ps1
+++ Task8/task8.ps1
@@ -1,10 +1,10 @@
 ﻿# Andrey Murzich
-# Task 7
-# Azure Recovery Vault Manipulation (Uses VM created in Task4)
+# Task 8
+# Web Application + SQL Database
 
 # Templates configuration
-$templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task7/index.json"
-$templateParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task7/parameters.json"
+$templateFile = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task8/index.json"
+$templateParameterUri = "https://raw.githubusercontent.com/ph5x5/AzureTemplates/master/Task8/parameters.json"
 $envPrefix = "learning4"
 $envLocation = "West Europe"
 
@@ -12,7 +12,9 @@ $envLocation = "West Europe"
 #Login-AzureRmAccount
 
 # Resource Group Deployment
+New-AzureRmResourceGroup -Name $envPrefix -Location $envLocation -Force
 New-AzureRmResourceGroupDeployment -ResourceGroupName $envPrefix `
                                    -TemplateFile $templateFile `
                                    -TemplateParameterUri $templateParameterUri `
                                    -Force -Verbose
+
