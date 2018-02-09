9ad20c444113cef5c25287a59d4393bd44259865
ihar.kotau <Ihar.Kotau@thomsonreuters.com>
2018-02-09 13:50:19 +0300

    Task7 31

--- Task7/restore.ps1
+++ Task7/restore.ps1
@@ -15,7 +15,7 @@ $storageAccountName = "csbf904e53bc8b7x4111x93b"
 
 $vault = Get-AzureRmRecoveryServicesVault -Name "$envPrefix-vault"
 Set-AzureRmRecoveryServicesVaultContext -Vault $vault
-$container = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM -Status Registered -FriendlyName "AzureBackup_$envPrefix-vm"
+$container = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM -Status Registered -FriendlyName "$envPrefix-vm"
 $backupItem = Get-AzureRmRecoveryServicesBackupItem -WorkloadType AzureVM -Container $container
 $RP = Get-AzureRmRecoveryServicesBackupRecoveryPoint -Item $backupItem -StartDate $startDate.ToUniversalTime() -EndDate $endDate.ToUniversalTime()
 $RestoreJob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $RP[0] -StorageAccountName $storageAccountName -StorageAccountResourceGroupName $resourceGroupStorage
